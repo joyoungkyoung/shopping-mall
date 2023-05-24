7650bd63f89949225f3eb2e2ff1690881d20fa5b
joyoungkyoung <dughks1130@gmail.com>
2023-05-24 10:40:13 +0900

    chore: mybatis -> type-aliases-package 설정

--- src/main/java/com/nicky/shoppingmall/config/MvcConfig.java
+++ src/main/java/com/nicky/shoppingmall/config/MvcConfig.java
@@ -10,6 +10,6 @@ public class MvcConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5500");
+        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500");
     }
 }
